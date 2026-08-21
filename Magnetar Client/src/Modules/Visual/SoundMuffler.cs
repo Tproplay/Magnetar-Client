ff9403bd911c5d6e0cee4ec27175e7f7b390adbd
Tproplay <tproplaygameplay@gmail.com>
2026-08-21 14:36:41 +0530

    added sound muffler

---
+++ Magnetar Client/src/Modules/Visual/SoundMuffler.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using System;
+
+namespace Magnetar_Client.Modules
+{
+    public class SoundMuffler : Module
+    {
+        // Mod Info
+        public override string Name { get; set; } = "Sound Muffler";
+        public override string Description { get; set; } = "Disable certain sounds from being played.";
+        public override string SearchHints { get; set; } = "soundmuffler audiofilter soundmuffle muteaudio " +
+            "soundblocker audiosettings volumecontrol soundmute noisecontrol disablesounds soundcleaner audioblock " +
+            "soundfix muteeffects audiomute soundmanager customaudio soundfilter audiodampener soundkiller";
+        public override ModuleCategory Category { get; set; } = ModuleCategory.Visual;
+
+        // Mod Data
+
+        public static SoundMuffler instance;
+
+        public MultiSelectSetting blacklistedTypes;
+
+        public SoundMuffler()
+        {
+            instance = this;
+
+            CreateCategory("General");
+
+            blacklistedTypes = new MultiSelectSetting("Blacklisted", typeof(SoundType))
+            {
+                CustomNames = TranslatedNames(typeof(SoundType))
+            };
+            AddSettings(blacklistedTypes);
+
+            EndCategory();
+        }
+
+        public override void OnLanguageChanged()
+        {
+            blacklistedTypes.CustomNames = TranslatedNames(typeof(SoundType));
+        }
+
+        // Mod Logic
+
+        [HarmonyPatch(typeof(GameAPP))]
+        public static class GameAppPatch
+        {
+            [HarmonyPatch(nameof(GameAPP.PlaySound), new Type[] { typeof(int), typeof(float), typeof(float) })]
+            [HarmonyPrefix]
+            public static bool PlaySoundIntPatch(int theSoundID, float theVolume, float pitch)
+            {
+                if (instance == null || !instance.Active ||
+                    !instance.blacklistedTypes.IsSelected(theSoundID)) return true;
+
+                return false;
+
+                
+            }
+        }
+        
+    }
+}

e2afdc41f9cec9ed1e110a1ab677ba7ce041ebd1
Tproplay <tproplaygameplay@gmail.com>
2026-09-20 14:29:12 +0530

    updated dlls 4.0

---
+++ Magnetar Client/References/Update_dlls.bat
@@ -0,0 +1,12 @@
+@echo off
+
+py update_dll.py "C:\Users\Tproplay\Games\Pvz\Fusion RH\English 4.0\MelonLoader\Il2CppAssemblies" "C:\Users\Tproplay\Desktop\Visual studio\Magnetar Client\Magnetar Client\References\Melonloader\Il2Cpp"
+
+py update_dll.py "C:\Users\Tproplay\Games\Pvz\Fusion RH\English 4.0\MelonLoader\net6" "C:\Users\Tproplay\Desktop\Visual studio\Magnetar Client\Magnetar Client\References\Melonloader"
+
+py update_dll.py "C:\Users\Tproplay\Games\Pvz\Fusion RH\UnityExploror 4.0\BepInEx\core" "C:\Users\Tproplay\Desktop\Visual studio\Magnetar Client\Magnetar Client\References\Bepinex"
+
+py update_dll.py "C:\Users\Tproplay\Games\Pvz\Fusion RH\UnityExploror 4.0\BepInEx\interop" "C:\Users\Tproplay\Desktop\Visual studio\Magnetar Client\Magnetar Client\References\Bepinex\Il2Cpp"
+
+echo All updates completed.
+pause
